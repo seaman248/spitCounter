f331f42bb8e4e89f02ec9e872f6acfd1be76a92e
seaman <semen.bko@ya.ru>
2014-12-28 00:55:11 +0300

    +one method

--- split.js
+++ split.js
@@ -3,4 +3,17 @@ function Spit () {
 		var date = new Date('DD-MM-YYYY');
 		db.put(date, reason);
 	}
+	this.getSpitReasons = function(db, dateS, dateE, cb){
+		var spits = []; // Spits is an array of spit objects, which contain date and reason properties
+		db.createReadStream({start: dateS, end: dateE}) // Get data from dateS(tart) to DateE(nd)
+			.on('data', function(data){
+				spits.push({date: data.key, reason: data.value});
+			})
+			.on('error', function(err){
+				cb(err);
+			})
+			.on('end', function(){
+				cb(null, spits);
+			})
+	}
 }
